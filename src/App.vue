f5493b2fc6ae79dc079983df1af8669224f30f91
yanglae <yanglae@hanmail.net>
2022-07-06 16:52:31 +0900

    블로그 글목록을 만들어보자 & 데이터바인딩, props 복습

--- src/App.vue
+++ src/App.vue
@@ -12,41 +12,66 @@
         <a class="nav-link disabled">Disabled</a>
       </div>
     </div>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
   </nav>
-  <div>https://velog.io/@do_ng_iill/npm-WARN-config-global-global-local-are-deprecated.-Use-locationglobal-instead.-%ED%95%B4%EA%B2%B0</div>
 
-  <div class="container">
-  <div class="row">
-    <div class="col-sm">
-      One of three columns
+  <div class="content">
+    <div class="container mb-5">
+      <h3>yang blog 라우터 공부</h3>
+      <p>Vue 로 만들었음</p>
     </div>
-    <div class="col-sm">
-      One of three columns
+    <hr />
+
+    <div class="mt-5 mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Email 주소</label>
+      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
     </div>
-    <div class="col-sm">
-      One of three columns
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label">내용</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+    </div>
+
+    <div class="d-grid gap-2 col-6 mx-auto">
+      <button class="btn btn-primary" type="button">완료</button>
+      <button class="btn btn-secondary" type="button">취소</button>
     </div>
   </div>
-</div>
+
+  <List v-for="write in 블로그글" :key="write"/>
+
 </template>
 
-<script>
 
+<script>
+import List from './components/List.vue';
+import blog from './assets/blog.js';
 
 export default {
   name: 'App',
+  data(){
+    return {
+      블로그글 : blog
+    }
+  },
   components: {
-    
+    List : List,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 맑은 고딕, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
+  letter-spacing: -0.5px;
+}
+.content{
+  margin: 100px;
 }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h5>블로그 글 제목</h5>
+    <p>블로그 글 날짜 2022.07.06</p>
+  </div>
+  <div>
+    <h5>블로그 글 제목</h5>
+    <p>블로그 글 날짜 2022.07.06</p>
+  </div>
+  <div>
+    <h5>블로그 글 제목</h5>
+    <p>블로그 글 날짜 2022.07.06</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+
+</style>
