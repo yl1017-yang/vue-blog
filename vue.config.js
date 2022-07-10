119a67b4130b3d83e90a14b4d720f6f024ab7ced
yanglae <yanglae@hanmail.net>
2022-07-10 15:24:48 +0900

    경로수정

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir:'./docs',
-  publicPath:'/vue-blog/',
+  publicPath:'/vue-blog',
 })
